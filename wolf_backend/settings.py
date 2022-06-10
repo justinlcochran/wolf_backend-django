cf9b7a4ca39c1d33e58d4c4f2a0b2e4396df70c3
JUSTINLCOCHRAN <justinlcochran@gmail.com>
2022-06-10 12:57:47 -0600

    corsall

--- wolf_backend/settings.py
+++ wolf_backend/settings.py
@@ -75,7 +75,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
     'wolfAPI',
 ]
 
